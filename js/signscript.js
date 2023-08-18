f31c3a1b5d839cb07f942ffcc6cae4a29de5d190
Matvey Kova <lica7747@gmail.com>
2023-08-18 04:49:09 +0300

    first commit

---
+++ js/signscript.js
@@ -0,0 +1,10 @@
+
+var login = "Admin";
+document.getElementById("button").addEventListener("click", function() {
+    var testlog = document.getElementById('log').value;
+    if(testlog = testlog){
+        console.log(testlog);
+        window.location="main.html";
+    }
+  });
+  
